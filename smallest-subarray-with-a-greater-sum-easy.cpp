eadca8f0cad0959366fc9231fc95e4d8f03d4b38
Yasser Mustafa <m.abdelkader.yasser@gmail.com>
2026-08-20 20:26:05 +0300

    practice: re-solve Smallest Subarray With a Greater Sum

--- smallest-subarray-with-a-greater-sum-easy.cpp
+++ smallest-subarray-with-a-greater-sum-easy.cpp
@@ -5,33 +5,26 @@ using namespace std;
 
 class Solution {
   public:
-    int findMinSubArray(int S, const vector<int>& arr) 
-    {
-      for (int i = 1; i <= arr.size(); i ++)
+    int findMinSubArray(int S, const vector<int>& arr) {
+      // TODO: Write your code here
+      int arrLength = arr.size() + 1;
+      int firstIndx = 0;
+      int windowSum = 0;
+      for(int endIdx = 0; endIdx < arr.size(); endIdx++)
       {
-        int startIndex = 0;
-        int windowSum = 0;
-        int maxWindowSum = 0;
-        for (int endIndex = 0; endIndex < arr.size(); endIndex++)
+        windowSum+= arr[endIdx];
+        while( windowSum >= S)
         {
-            windowSum += arr[endIndex];
-            if (endIndex >= i-1)
-            {
-                maxWindowSum = max(maxWindowSum, windowSum);
-                windowSum -= arr[startIndex++];
-
-            }
+          arrLength = min(arrLength, endIdx - firstIndx + 1);
+          windowSum -= arr[firstIndx];
+          firstIndx++;
         }
-
-        if(maxWindowSum >= S)
-        {
-                return i;
-        }
-
       }
-      return 0;
+
+      return arrLength == arr.size()+1 ? 0 : arrLength;
     }
   };
+
 
   int main(int argc, char *argv[])
 {
@@ -39,6 +32,8 @@ class Solution {
     int result = sol.findMinSubArray(7, vector<int>{2, 1, 5, 2, 3, 2});
     cout << "Smallest subarray length: " << result << endl;
     result = sol.findMinSubArray(7, vector<int>{2, 1, 5, 2, 8});
+    cout << "Smallest subarray length: " << result << endl;
+    result = sol.findMinSubArray(8, vector<int>{3, 4, 1, 1, 6});
     cout << "Smallest subarray length: " << result << endl;
     result = sol.findMinSubArray(8, vector<int>{3, 4, 1, 1, 6});
     cout << "Smallest subarray length: " << result << endl;
