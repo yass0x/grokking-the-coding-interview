5d314d16ef32cb3fae43da5351a0e35a74dec7e2
Yasser Mustafa <m.abdelkader.yasser@gmail.com>
2026-08-21 21:48:24 +0300

    practice: solve Subsets

--- subsets-easy.cpp
+++ subsets-easy.cpp
@@ -7,11 +7,22 @@ class Solution {
 public:
   vector<vector<int>> findSubsets(const vector<int>& nums) {
     vector<vector<int>> subsets;
+    subsets.push_back(vector<int>());
     // TODO: Write your code here
+    for (int  i = 0; i < nums.size(); i++)
+    {
+        int subsetsCurrentSize = subsets.size();
+        for (int j = 0; j < subsetsCurrentSize; j++)
+        {
+            subsets.push_back(subsets[j]);
+            subsets.back().push_back(nums[i]);
+        }   
+    }
+    
     return subsets;
   }
 };   
- 
+
 int main(int argc, char* argv[]) {
   Solution sol;
   vector<vector<int>> result = sol.findSubsets(vector<int>{1, 3});
