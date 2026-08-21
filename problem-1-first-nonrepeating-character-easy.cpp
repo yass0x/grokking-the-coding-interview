a2dff7943d1130737310628aa357ff008f017799
Yasser Mustafa <m.abdelkader.yasser@gmail.com>
2026-08-21 20:31:59 +0300

    solve problem-1-first-nonrepeating-character-easy

--- problem-1-first-nonrepeating-character-easy.cpp
+++ problem-1-first-nonrepeating-character-easy.cpp
@@ -6,7 +6,23 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         // ToDo: Write Your Code Here.
-        return -1;
+        unordered_map<char, int> charFreqMap;
+        int firstNonRepeatingCharIdx = -1;
+        for (int i = 0; i < s.length(); i++)
+        {
+            charFreqMap[s[i]] ++;
+        }
+        for (int  i = 0; i < s.length(); i++)
+        {
+            if(charFreqMap[s[i]] == 1)
+            {
+               firstNonRepeatingCharIdx = i; 
+               break;
+            } 
+        }
+        
+
+        return firstNonRepeatingCharIdx;
     }
 };
 int main() {
@@ -14,4 +30,5 @@ int main() {
     cout << sol.firstUniqChar("apple") << endl;  // Expected: 0
     cout << sol.firstUniqChar("abcab") << endl;  // Expected: 2
     cout << sol.firstUniqChar("abab") << endl;   // Expected: -1
+    cout << sol.firstUniqChar("swiss") << endl;   // Expected: 1
 }
