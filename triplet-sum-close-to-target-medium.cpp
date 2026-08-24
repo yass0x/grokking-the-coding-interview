b44594d2946f50a4ccd3e0d81ccf7e2b55ca40f2
Mustafa, Yasser <yasser.mustafa1@aptiv.com>
2026-08-24 20:19:50 +0300

    WIP

--- triplet-sum-close-to-target-medium.cpp
+++ triplet-sum-close-to-target-medium.cpp
@@ -9,7 +9,56 @@ class Solution {
 public:
   int searchTriplet(vector<int>& arr, int targetSum) {
     // TODO: Write your code here
-    return 0;
+    sort(arr.begin(), arr.end());
+    int minSum = std::numeric_limits<int>::max();
+
+    for (int  i = 0; i < arr.size() -1; i++)
+    {
+        if (arr[i] > 0 && arr[i] == arr[i-1])
+        {
+            continue;
+        } 
+        int currentMinSum = findCurrentMinClosestSumToTarget(arr, -(targetSum - arr[i]), i + 1);
+        minSum = min(minSum, currentMinSum); 
+    }
+    
+    return minSum;
+  }
+
+  private:
+   static int findCurrentMinClosestSumToTarget(vector<int> arr, int targetSum, int leftIdx)
+  {
+    int rightIdx = arr.size() - 1;
+
+    int minDiff = std::numeric_limits<int>::max();
+    while (leftIdx < rightIdx)  
+    {
+        int currentSum = arr[leftIdx] + arr[rightIdx];
+        if (targetSum > currentSum)
+        {
+            minDiff = (minDiff, targetSum - currentSum);
+            leftIdx++;
+        }
+        else if(targetSum < currentSum)
+        {
+            minDiff = (minDiff, currentSum- targetSum);
+            rightIdx--;
+        }
+        else
+        {
+            minDiff = 0;
+            break;
+        }
+        while (leftIdx < rightIdx && arr[leftIdx] == arr[leftIdx -1])
+        {
+            leftIdx++;
+        }
+        while (leftIdx < rightIdx && arr[rightIdx] == arr[rightIdx +1])
+        {
+            rightIdx--;
+        }
+    }   
+    return minDiff;
   }
 };
 
