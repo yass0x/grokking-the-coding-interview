6b00bfdb7bacdf03916b8473f671e5793d35dc0d
Mustafa, Yasser <yasser.mustafa1@aptiv.com>
2026-08-30 15:37:04 +0300

    Added Problem Description

---
+++ start-of-linkedlist-cycle-medium.cpp
@@ -0,0 +1,47 @@
+using namespace std;
+#include <iostream>
+
+class ListNode {
+public:
+  int val = 0;
+  ListNode *next;
+
+  ListNode(int value) {
+    this->val = value;
+    next = nullptr;
+  }
+};
+
+class Solution {
+public:
+  ListNode *findCycleStart(ListNode *head) {
+    // TODO: Write your code here
+    return head;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  Solution sol;
+  ListNode *head = new ListNode(1);
+  head->next = new ListNode(2);
+  head->next->next = new ListNode(3);
+  head->next->next->next = new ListNode(4);
+  head->next->next->next->next = new ListNode(5);
+  head->next->next->next->next->next = new ListNode(6);
+
+  // Create a cycle by connecting nodes
+  head->next->next->next->next->next->next = head->next->next;
+  cout << "LinkedList cycle start: " 
+      << sol.findCycleStart(head)->val << endl;
+
+  // Create a different cycle
+  head->next->next->next->next->next->next = head->next->next->next;
+  cout << "LinkedList cycle start: " 
+      << sol.findCycleStart(head)->val << endl;
+
+  // Create a cycle that points back to the head
+  head->next->next->next->next->next->next = head;
+  cout << "LinkedList cycle start: " 
+      << sol.findCycleStart(head)->val << endl;
+}
+
